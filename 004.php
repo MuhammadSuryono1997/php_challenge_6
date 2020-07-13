8f8e2dc09fc016ae758e57dcd42415e486017a3a
MuhammadSuryono1997 <msuryono0@gmail.com>
2020-07-13 16:56:28 +0900

    functional programming

--- 004.php
+++ 004.php
@@ -1,16 +1,18 @@
 <?php
 function Mode($data)
 {
+    $data = join(' ',$data);
+    $new_data = explode(' ', $data);
     $cg = function($data){
         $values = array_count_values($data);
         $mode = array_search(max($values), $values);
         return $mode;
     };
 
-    return $cg($data);
+    return $cg($new_data);
 }
 $numbers = [1,2,3,4,5,6,6,8,8,6,9];
-$grades = ["87.5", "88.5", "60.5", "90.5", "88.5"];
+$grades = [87.5, 88.5, 60.5, 90.5, 88.5];
 
 echo Mode($numbers)."\n";
 echo Mode($grades)."\n";
